f2e35c63a2bbd1bc0618b9aee803d5db4cdfde46
geetanjai <81380894+geetanjai@users.noreply.github.com>
2021-06-23 21:47:18 +0530

    first

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileLocation = "clients.xml";
+
+            string xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?><SessionId><Source></Source></SessionId>";
+            XDocument doc = XDocument.Parse(xml);
+            XElement source = doc.Descendants("Source").FirstOrDefault();
+            source.Add(new XElement("User", new object[] {
+new XAttribute("username", "AqB"),
+new XElement("DOB", "25/5/1981"),
+new XElement("FirstName", "AVS"),
+new XElement("LastName", "WDW"),
+new XElement("Location", "FWAWE")
+}));
+            source.Save(fileLocation);
+            Console.WriteLine("Done");
+        
+    }
+    }
+}
